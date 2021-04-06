00697160538e8457997a142628381281108dda6b
Levi Ermonaites De Freitas <68472819+leviermonaites@users.noreply.github.com>
2021-04-06 18:50:17 -0300

    Refactoring Clock challenge

--- clock.js
+++ clock.js
@@ -1,8 +1,9 @@
 // Match the hour no matter the pattern that it's established.
-const test = 'Published by: Levi Ermonaites, Brazil, Sao Paulo, 4:50AM';
+const test =
+  'Published by: Levi Ermonaites, Brazil, Sao Paulo, 10:50AM - 04/05/2021';
 
 console.log(
   test.match(
-    /\b(((([0-1]?[0-2])|[0]?[0-9]):[0-5]?[0-9] *[AP]M)|(([0-1]?[0-9])|(2[0-3])):[0-5]?[0-9])[-,.]?\b/gi
+    /\b((([0-1]?[0-2]|[0]?[0-9]):[0-5]?[0-9] *[AP]M)|(([0-1]?[0-9])|(2[0-3])):[0-5]?[0-9])[-,.]?\b/gi
   )
 );
